41b2bf40ea562811b3d1587c98e0f2a0aae845bc
ericjair <ericjairlome@gmail.com>
2024-03-07 15:45:49 -0600

    practica00

--- practica00/src/main/java/mx/uv/App.java
+++ practica00/src/main/java/mx/uv/App.java
@@ -1,18 +1,34 @@
 package mx.uv;
 
 import static spark.Spark.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+            return "OK";
+        });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+        System.out.println("Hello World!");
 
         get("/xml", (request, response) -> {
-            String respuesta = "<x><y/></x>";
+            String respuesta =
+            "<p>"+ 
+            "<x><y>algo<y/></x>"+
+            "<x><y>algo<y/></x>";
             response.type("application/xml");
             return respuesta;
         });
